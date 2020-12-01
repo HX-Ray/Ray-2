ef771fb5336f8bcc7f4ca2fcbad295fc4c3fce63
HX-Ray <1923642727@qq.com>
2020-12-01 22:38:49 +0800

    1201

--- p.go
+++ p.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-		requestUrl := "https://manhua.dmzj.com/bianfuxiav3"
+	var keyword string
+	fmt.Scanln(&keyword)
+
+	//num := 3
+
+	for i := 1; i <= 5; i++ {
+		requestUrl := "https://www.dmzj.com/dynamic/o_search/index/"+ keyword + "/" + strconv.Itoa(i)
+
 		response, err := http.Get(requestUrl)
 		if err != nil {
 			panic(err)
@@ -19,6 +30,20 @@ func main() {
 		}
 		defer response.Body.Close()
 		content := string(body)
-		fmt.Println(content)
 
+		dom, err := goquery.NewDocumentFromReader(strings.NewReader(content))
+		if err != nil {
+			panic(err)
+		}
+
+		dom.Find(".update_con.autoHeight").Each(func(i int, selection *goquery.Selection) {
+			// fmt.Println(selection.Text())
+			selection.Find("p").Each(func(i int, title *goquery.Selection) {
+				// fmt.Println(title.Text())
+				// fmt.Printf("%3d   ", num/3)
+				fmt.Println(title.Text())
+				// num++
+			})
+		})
+	}
 }
